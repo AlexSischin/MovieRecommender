562f80d940662a7168e2b0decb4dbf579c699126
Alex Shishkin <alexsischin@gmail.com>
2023-03-22 16:56:13 +0200

    Routine for comparing distributions

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 colors = ['#fd7f6f', '#7eb0d5', '#b2e061', '#bd7ebe', '#ffb55a', '#ffee65', '#beb9db', '#fdcce5', '#8bd3c7']
 l_styles = ['solid', 'dotted', 'dashed', 'dashdot']
@@ -22,5 +23,26 @@ def compare_history(history_list, metrics):
             metric_hist = history_metrics[metric]
             label = f'{model} - {metric}'
             ax.plot(epoch, metric_hist, label=label, color=color, linestyle=style)
+    fig.legend()
+    plt.show()
+
+
+def compare_distributions(true, predictions, bins=10, rwidth=0.7):
+    fig, ax = get_single_axes()
+    ax.set_title('Output distribution')
+    ax.set_xlabel('Value')
+    ax.set_ylabel('Density')
+
+    if predictions:
+        labels, data, cols = list(zip(*[[a[0], np.array(a[1]).flatten(), c] for a, c in zip(predictions, colors)]))
+        labels, data, cols = list(labels), list(data), list(cols)
+    else:
+        labels, data, cols = [], [], []
+
+    labels.insert(0, 'True')
+    data.insert(0, np.array(true).flatten())
+    cols.insert(0, '#333333')
+
+    ax.hist(data, bins=bins, density=True, color=cols, alpha=1, rwidth=rwidth, label=labels)
     fig.legend()
     plt.show()
